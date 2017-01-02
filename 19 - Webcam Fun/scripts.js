86e3cec3d8dab6cc59b72d050b0b69ed03f0095e
Daniel Nieto <danielnieto89@gmail.com>
2017-01-01 23:23:15 -0600

    apply several functions to modify the pixel data

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -20,8 +20,62 @@ function paintToCanvas(){
 
     return setInterval(()=>{
         ctx.drawImage(video, 0,0, width, height);
+        let pixels = ctx.getImageData(0, 0, width, height);
+        // pixels = redEffect(pixels);
+        pixels = rgbSplit(pixels);
+        // pixels = greenScreen(pixels);
+
+        ctx.putImageData(pixels, 0, 0);
+
+
     }, 16);
 
+}
+
+function redEffect(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 200; // red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // green
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // red
+    pixels.data[i + 500] = pixels.data[i + 1]; // green
+    pixels.data[i - 550] = pixels.data[i + 2]; // blue
+  }
+  return pixels;
+}
+
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // make it transparent
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
 }
 
 function takePhoto(){
